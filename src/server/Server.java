e7c34c6c3c9f1bbc12442902857da5285aad1900
AlexEvdokimovSpb <3416216@gmail.com>
2020-09-10 02:47:04 +0300

    HW7

--- src/server/Server.java
+++ src/server/Server.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
@@ -13,6 +14,7 @@ import java.util.Vector;
 public class Server {
     private List<ClientHandler> clients;
     private AuthService authService;
+    private HashMap<String, ClientHandler> hashClients; // ключ - ник, значение - клиент
 
     private int PORT = 8189;
     ServerSocket server = null;
@@ -21,6 +23,7 @@ public class Server {
     public Server(){
         clients = new Vector<>();
         authService = new SimpleAuthService();
+        hashClients = new HashMap<>();
 
         try {
             server = new ServerSocket(PORT);
@@ -29,7 +32,6 @@ public class Server {
             while (true) {
                 socket = server.accept();
                 System.out.println("Клиент подключился");
-
                 new ClientHandler(this, socket);
             }
 
@@ -55,12 +57,30 @@ public class Server {
         }
     }
 
+    public void addressMsg(ClientHandler sender, String addressNickname, String msg){ // метод отправки оп адресу
+        ClientHandler address = hashClients.get(addressNickname);
+        String message = String.format("%s : %s", sender.getNickname(), msg);
+        address.sendMsg(message);
+    }
+
+    public boolean thisNicknameInChat(String addressNickname){ // метод проверки присутствия в чате
+        return (hashClients.containsKey(addressNickname));
+    }
+
+
     public void subscribe(ClientHandler clientHandler){
         clients.add(clientHandler);
     }
 
     public void unsubscribe(ClientHandler clientHandler){
         clients.remove(clientHandler);
+    }
+
+    public void subscribeNickname(String nickname, ClientHandler clientHandle)
+    { hashClients.put (nickname, clientHandle); }
+
+    public void unsubscribeNickname(String nickname){
+        hashClients.remove(nickname);
     }
 
 }
